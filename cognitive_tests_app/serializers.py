d3a3056c7b2c190fec2432b8489f5fc649e496b0
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2025-02-28 15:53:32 +0300

    4. Реализовала сохранение результатов тестов в базу данных.

--- cognitive_tests_app/serializers.py
+++ cognitive_tests_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import AppUser, Profile
+from .models import AppUser, Profile, Result, Test
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,3 +21,16 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
+class ResultSerializer(serializers.ModelSerializer):
+    test = serializers.PrimaryKeyRelatedField(queryset=Test.objects.all())  # Используем PrimaryKeyRelatedField
+    user = serializers.PrimaryKeyRelatedField(queryset=AppUser.objects.all())
+
+    class Meta:
+        model = Result
+        fields = ['test', 'user', 'score_percentage']
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ['id', 'title', 'description']
--- cognitive_tests_app/urls.py
+++ cognitive_tests_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from .views import RegisterView, LoginView, ProfileView, ResultView, TestListView, TestDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('result/', ResultView.as_view(), name='result'),
+    path('tests/', TestListView.as_view(), name='test-list'),
+    path('tests/<int:test_id>/', TestDetailView.as_view(), name='test-detail'),
 ]
